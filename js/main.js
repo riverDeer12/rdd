ade625be99298451cf9a38e9ba4edc9a31b07765
ninatrbojevic <nina.trbojevic@student.uniri.hr>
2026-07-25 14:27:11 +0200

    added apex performance project and fixes bugs

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@
         opacity: 0,
         duration: 1000,
         begin: function(anim) {
-            window.scrollTo(0, 0);
+            if (!window.location.hash) window.scrollTo(0, 0);
         }
     })
     .add({
